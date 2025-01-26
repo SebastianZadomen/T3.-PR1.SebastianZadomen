bcbcfbe27545e026ad7bcdde20827bd48801fd62
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2025-01-26 19:20:41 +0100

    Closes #1 "Implemented Class Diagram "

---
+++ T3Pr1SebastianZadomen/HydroelectricSystem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3Pr1SebastianZadomen
+{
+    public class HydroelectricSystem : EnergySystem
+    {
+    }
+}
