0d5056f2d6dbacff2ee49c2d0fef9f7a0070fab3
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2025-01-26 21:08:07 +0100

    Closes #18 "New Class Simulated Result"

---
+++ T3Pr1SebastianZadomen/SimulationResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3Pr1SebastianZadomen
+{
+    public class SimulationResult
+    {
+        public string SystemType { get; set; }  
+        public string Date { get; set; }        
+        public double EnergyGenerated { get; set; } 
+
+
+        public SimulationResult(string systemType, string date, double energyGenerated)
+        {
+            SystemType = systemType;
+            Date = date;
+            EnergyGenerated = energyGenerated;
+        }
+
+        public override string ToString()
+        {
+            return $"System: {SystemType}, Date: {Date}, Energy Generated: {EnergyGenerated} kWh";
+        }
+    }
+}
