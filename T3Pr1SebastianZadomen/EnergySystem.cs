dc4d61d5605e34e064127a62685f57ea7dfaa7c1
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2025-01-26 19:26:08 +0100

    Closes #4 "Class Energy System Added"

--- T3Pr1SebastianZadomen/EnergySystem.cs
+++ T3Pr1SebastianZadomen/EnergySystem.cs
@@ -7,5 +7,17 @@ namespace T3Pr1SebastianZadomen
 {
     public abstract class EnergySystem
     {
+            public int Id { get; set; }
+            public string Date { get; set; }
+            public double EnergyGenerated { get; set; }
+
+            public EnergySystem()
+            {
+               
+                Date = Program.RequestDate();
+            }
+
+            public abstract void Simulate();
+        
     }
 }
